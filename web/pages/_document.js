9b7164992e984115cb29f0205e3325d1dccd33a4
Sadman Shawmik <hello@sadmansh.com>
2020-12-30 04:45:23 +0600

    Fix meta tags and add document

---
+++ web/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx)
+		return { ...initialProps }
+	}
+
+	render() {
+		return (
+			<Html lang="en">
+				<Head />
+				<body>
+					<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPWXS2"
+					height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
+}
+
+export default MyDocument
--- web/pages/index.js
+++ web/pages/index.js
@@ -8,15 +8,14 @@ const Index = () => {
 		<>
 			<MainHead>
 				<meta charset="UTF-8" />
-				<meta name=description content="Software Engineer. This is my portfolio and blog." />
-				<meta charset=utf-8 />
-				<meta property=og:title content="Sadman Shawmik" />
-				<meta property=og:description content="Software Engineer. This is my portfolio and blog." />
-				<meta property=og:type content=website />
-				<meta property=og:image content=assets/images/hi.png />
-				<meta name=twitter:card content=summary_large_image />
-				<meta name=twitter:title content="Sadman Shawmik" />
-				<meta name=twitter:description content="Software Engineer. This is my portfolio and blog." />
+				<meta name="description" content="Software Engineer. This is my portfolio and blog." />
+				<meta property="og:title" content="Sadman Shawmik" />
+				<meta property="og:description" content="Software Engineer. This is my portfolio and blog." />
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content="/hi.png" />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="Sadman Shawmik" />
+				<meta name="twitter:description" content="Software Engineer. This is my portfolio and blog." />
 				<title>Sadman Shawmik - Software Engineer</title>
 			</MainHead>
 			<Main>
