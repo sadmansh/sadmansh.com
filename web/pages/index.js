57400b957a4a00b37918d83fa4ba421a04ad9fcc
Sadman Shawmik <hello@sadmansh.com>
2020-12-30 04:36:48 +0600

    Add meta tags and gtm code

--- web/pages/index.js
+++ web/pages/index.js
@@ -7,6 +7,16 @@ const Index = () => {
 	return (
 		<>
 			<MainHead>
+				<meta charset="UTF-8" />
+				<meta name=description content="Software Engineer. This is my portfolio and blog." />
+				<meta charset=utf-8 />
+				<meta property=og:title content="Sadman Shawmik" />
+				<meta property=og:description content="Software Engineer. This is my portfolio and blog." />
+				<meta property=og:type content=website />
+				<meta property=og:image content=assets/images/hi.png />
+				<meta name=twitter:card content=summary_large_image />
+				<meta name=twitter:title content="Sadman Shawmik" />
+				<meta name=twitter:description content="Software Engineer. This is my portfolio and blog." />
 				<title>Sadman Shawmik - Software Engineer</title>
 			</MainHead>
 			<Main>
