d91ece69a74f3c2eef52c0521f1e35db4c6bc49f
Sadman Shawmik <hello@sadmansh.com>
2020-12-30 04:54:38 +0600

    Fix iframe style in document

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,18 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx)
-		return { ...initialProps }
-	}
-
 	render() {
 		return (
 			<Html lang="en">
 				<Head />
 				<body>
 					<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPWXS2"
-					height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+					height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
 					<Main />
 					<NextScript />
 				</body>
