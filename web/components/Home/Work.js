fd72ecc12a292726dda090bc464d04e3487c55a0
Sadman Shawmik <hello@sadmansh.com>
2021-01-26 04:13:22 +0600

    Fix className errors

--- web/components/Home/Work.js
+++ web/components/Home/Work.js
@@ -5,27 +5,61 @@ const Work = () => {
 		<section className={styles.work} id="work">
 			<div className="container">
 				<h2>My Work</h2>
-				<p>When I am working as an independent contractor, I usually help out agencies with the more complicated parts of their projects and sometimes the whole project itself. This is a list of some of those and also some of my own side projects.</p>
+				<p>
+					When I am working as an independent contractor, I usually help out agencies with the more
+					complicated parts of their projects and sometimes the whole project itself. This is a list of some
+					of those and also some of my own side projects.
+				</p>
 
-				<ul class="project-list">
+				<ul className="project-list">
 					<li>
-						<a href="https://www.ricemedia.co" rel="noopener noreferrer" target="_blank" id="work-url-rice">Rice</a>
+						<a href="https://www.ricemedia.co" rel="noopener noreferrer" target="_blank" id="work-url-rice">
+							Rice
+						</a>
 						<p>WordPress, JavaScript</p>
 					</li>
 					<li>
-						<a href="https://entertainment.directv.com" rel="noopener noreferrer" target="_blank" id="work-url-directv">DIRECTV Entertainment Hub</a>
+						<a
+							href="https://entertainment.directv.com"
+							rel="noopener noreferrer"
+							target="_blank"
+							id="work-url-directv"
+						>
+							DIRECTV Entertainment Hub
+						</a>
 						<p>WordPress, JavaScript</p>
 					</li>
 					<li>
-						<a href="https://ourheartlands.pluralartmag.com" rel="noopener noreferrer" target="_blank" id="work-url-heartlands">Heartlands</a>
+						<a
+							href="https://ourheartlands.pluralartmag.com"
+							rel="noopener noreferrer"
+							target="_blank"
+							id="work-url-heartlands"
+						>
+							Heartlands
+						</a>
 						<p>React, WordPress REST API</p>
 					</li>
 					<li>
-						<a href="https://github.com/sadmansh/snapcharge" rel="noopener noreferrer" target="_blank" id="work-url-snapcharge">SnapCharge</a>
+						<a
+							href="https://github.com/sadmansh/snapcharge"
+							rel="noopener noreferrer"
+							target="_blank"
+							id="work-url-snapcharge"
+						>
+							SnapCharge
+						</a>
 						<p>React, Redux, Node</p>
 					</li>
 					<li>
-						<a href="https://www.asianfilmarchive.org" rel="noopener noreferrer" target="_blank" id="work-url-afa">Asian Film Archive</a>
+						<a
+							href="https://www.asianfilmarchive.org"
+							rel="noopener noreferrer"
+							target="_blank"
+							id="work-url-afa"
+						>
+							Asian Film Archive
+						</a>
 						<p>WordPress</p>
 					</li>
 				</ul>
--- web/components/Layout/Footer.js
+++ web/components/Layout/Footer.js
@@ -2,11 +2,19 @@ import styles from '../../styles/Footer.module.scss'
 
 const Footer = () => (
 	<footer className={styles.footer}>
-		<div class="container">
-			<ul class="social-links">
-				<li><a aria-current="page" class="" href="/">About</a></li>
-				<li><a href="/blog">Blog</a></li>
-				<li><a href="/rss.xml">RSS</a></li>
+		<div className="container">
+			<ul className="social-links">
+				<li>
+					<a aria-current="page" href="/">
+						About
+					</a>
+				</li>
+				<li>
+					<a href="/blog">Blog</a>
+				</li>
+				<li>
+					<a href="/rss.xml">RSS</a>
+				</li>
 			</ul>
 		</div>
 	</footer>
