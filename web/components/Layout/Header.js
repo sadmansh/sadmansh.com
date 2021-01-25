7af980b3c2109f4684f5bea842f74cbd8c30ef7f
Sadman Shawmik <hello@sadmansh.com>
2021-01-26 05:31:02 +0600

    Fix nav brand link

--- web/components/Layout/Header.js
+++ web/components/Layout/Header.js
@@ -4,11 +4,13 @@ import styles from '../../styles/Header.module.scss'
 const Header = () => (
 	<header className={styles.header}>
 		<div className="container">
-			<Link href="/" className={styles.navBrand}>
-				<svg width="32" height="32" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<circle cx="20" cy="20" r="20" fill="#B4F7F2"></circle>
-				</svg>
-				<span>Sadman Shawmik</span>
+			<Link href="/">
+				<a className={styles.navBrand}>
+					<svg width="32" height="32" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<circle cx="20" cy="20" r="20" fill="#B4F7F2"></circle>
+					</svg>
+					<span>Sadman Shawmik</span>
+				</a>
 			</Link>
 			<nav>
 				<Link href="/">About</Link>
