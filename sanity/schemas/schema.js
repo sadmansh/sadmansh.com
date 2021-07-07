07da8605e202c16bf58c469e6cca633c67396b07
Sadman Shawmik <sadman@industrydive.com>
2021-07-07 20:09:27 +0600

    Add snippet schema to sanity

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -9,6 +9,7 @@ import blockContent from './blockContent'
 import category from './category'
 import tag from './tag'
 import post from './post'
+import snippet from './snippet'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -20,6 +21,7 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     post,
+    snippet,
     category,
     tag,
     // When added to this list, object types can be used as
