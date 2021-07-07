997d4f581723960e8088838dc6cbc2043dc58df1
Sadman Shawmik <sadman@industrydive.com>
2021-07-08 01:08:53 +0600

    Show tags in blog page

--- web/pages/blog/index.js
+++ web/pages/blog/index.js
@@ -26,7 +26,7 @@ const Index = ({ posts }) => {
 }
 
 export const getStaticProps = async (context) => {
-	const data = await getAllPosts('post')
+	const data = await getAllPosts()
 	return {
 		props: {
 			posts: data
